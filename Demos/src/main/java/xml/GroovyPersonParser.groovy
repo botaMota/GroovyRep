70fcf8347be1b33530ba2b37923eb533dd758699
[a609848] El Moutaouakil BOUTAHIR <el-moutaouakil.boutahir@atos.net>
2016-04-14 15:10:25 +0100

    ddd

--- Demos/src/main/java/xml/GroovyPersonParser.groovy
+++
@@ -1,6 +0,0 @@
-package xml
-
-def root = new XmlSlurper().parse('../../resources/people.xml')
-println "the second name is ${root.person[1].name}"
-println "the second name is ${root.person[1].@id}"
-
